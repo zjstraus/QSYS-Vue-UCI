bd683493a0e87e6f685d6845fada4bfeaca1e89e
Zach Strauss <zach@strauss.audio>
2020-11-27 14:59:30 -0500

    Add license

--- src/components/BaseLED.vue
+++ src/components/BaseLED.vue
@@ -1,3 +1,17 @@
+<!--Copyright (C) 2020  Zach Strauss-->
+
+<!--This program is free software: you can redistribute it and/or modify-->
+<!--it under the terms of the GNU General Public License as published by-->
+<!--the Free Software Foundation, version 3.-->
+
+<!--This program is distributed in the hope that it will be useful,-->
+<!--but WITHOUT ANY WARRANTY; without even the implied warranty of-->
+<!--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the-->
+<!--GNU General Public License for more details.-->
+
+<!--You should have received a copy of the GNU General Public License-->
+<!--along with this program.  If not, see <https://www.gnu.org/licenses/>.-->
+
 <template>
   <div class="LED" v-bind:style="{ backgroundColor: Color }">
   </div>
